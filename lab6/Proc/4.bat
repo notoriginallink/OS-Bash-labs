bd87730f83c39c1054e8b8679e857e12136ee82b
notoriginallink <toldan04@yandex.ru>
2024-05-10 22:09:22 +0300

    fix: some fc path errors

--- lab6/Proc/4.bat
+++ lab6/Proc/4.bat
@@ -4,19 +4,23 @@ set "PROC_NAME=3.bat"
 set FILE="C:\WINDOWS\system32\shell32.dll"
 for /f "tokens=*" %%i in ('hostname') do set "HOSTNAME=%%i"
 
-
 :: tasklist with /FI filters by IMAGENAME (in that case) then 'find' tries to find proccess
 :: if process is found, then %ERRORLEVEL% is set to 0 (because find not failed)
+@:loop
+@tasklist /FI "IMAGENAME eq xcopy" 2>NUL | find /I /N "xcopy">NUL
+@if "%ERRORLEVEL%"=="0" goto :found
+@goto loop
 
-xcopy xcopy "%FILE%" \\%HOSTNAME%\temp
+@:found
+
 :: kill process by its IMAGENAME
 taskkill /IM xcopy
 
 :: shows difference between two files
-fc %FILE% \\%HOSTNAME%\temp > diff1.txt
+fc %FILE% %HOSTNAME%\temp > diff1.txt
 
-xcopy "%FILE%" \\%HOSTNAME%\temp
+xcopy /Z "%FILE%" \\%HOSTNAME%\temp
 
-fc %FILE% \\%HOSTNAME%\temp > diff2.txt
+fc %FILE% %HOSTNAME%\temp > diff2.txt
 
-pause
+exit /b
